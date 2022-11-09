cd51e6d33ce41302f8fc1d396883876d46fe8d1d
Sagor Rana <sagorranait@gmail.com>
2022-11-10 02:21:58 +0600

    Functionality of showing reviews using email is done.

--- src/pages/Reviews/Reviews.js
+++ src/pages/Reviews/Reviews.js
@@ -1,12 +1,25 @@
+import { useContext, useEffect, useState } from 'react';
 import Review from '../../components/Review/Review'
+import { StateContext } from '../../StateProvider';
 
 const Reviews = () => {
+  const { user } = useContext(StateContext);
+  const [userReviews, setUserReviews] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/review?email=${user?.email}`)
+    .then(res => res.json())
+    .then(data => {
+      setUserReviews(data);
+    })
+    .catch((error)=> console.log(error));
+  }, [user?.email])
+
   return (
     <div className='reviews-area outlet-page'>
       <h2>My Reviews</h2>
       <div className='reviews'>
-         <Review/>
-         <Review/>
+          {userReviews.map(review => <Review key={review._id} buttons={true} data={review} />)}
       </div>
     </div>
   )
