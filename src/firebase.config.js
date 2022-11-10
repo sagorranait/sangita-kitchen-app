ee56b4888d543d33e8f624c6ee276de8d96b418f
Sagor Rana <sagorranait@gmail.com>
2022-11-10 16:34:30 +0600

    Update firebase config.

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGEBUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
     appId: process.env.REACT_APP_APPID
-  };
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
