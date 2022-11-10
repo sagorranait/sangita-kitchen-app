1187c52b6199c77c051022d6fc99b66bccd66785
Sagor Rana <sagorranait@gmail.com>
2022-11-10 11:22:49 +0600

    Simple challenge - Sorting functionality is done.

--- src/components/ServiceReviews/ServiceReviews.js
+++ src/components/ServiceReviews/ServiceReviews.js
@@ -2,11 +2,15 @@ import Review from '../Review/Review';
 import './ServiceReviews.css';
 
 function ServiceReviews({reviewData}) {
+  const sortReviews = reviewData.sort((a, b)=>{
+    return b.date.localeCompare(a.date);
+  });
+
    return (
     <div className='service-reviews'>
       <h2>Reviews ({reviewData.length})</h2>
       {reviewData.length !== 0 ? 
-         reviewData.map((reviews, index) => <Review key={index} buttons={false} data={reviews} />)
+         sortReviews.map((review, index) => <Review key={index} buttons={false} data={review} />)
          : <h6>No reviews for this service.</h6>}
     </div>
   )
